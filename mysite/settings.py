fcf583a7a4127f3d5a1f0839d495eb74886084d9
Camila <camilamazza4@hotmail.com>
2018-11-22 14:27:16 -0300

    arreglé local host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q7edhj!7np(kn$_7(pf_#)uqn0bd0-+2+qazaav1x)l4x45r^y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<tu-usuario>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tutodjangocami.pythonanywhere.com']
 
 
 # Application definition
